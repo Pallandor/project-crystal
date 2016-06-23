c17602d7811770a1d203af3f24b685e56c202e5a
Pallandor <roger.sejas@gmail.com>
2016-06-23 18:08:35 +0930

    Refactor add-user API route logic

--- server/db/controllers/users.js
+++ server/db/controllers/users.js
@@ -36,9 +36,20 @@ module.exports = rep => {
     findById: id =>
       rep.oneOrNone(sql.findById, id, user =>
         user),
+
     findByEmail: email =>
       rep.oneOrNone(sql.findByEmail, email, user =>
         user),
+
+    checkIfExists: email => 
+      rep.oneOrNone(sql.findByEmail, email, user =>{
+        // const helpers = require(__dirname + '/../../helpers/helpers');
+        // helpers.customLog(user);
+        if (user !== null){
+          return true;
+        }
+        return false;
+      }),
 
     // RF: 
     // update: (user_id, objWithUpdates, pgp) => {

55dcf487c4dd7003ce54c2482e8af2ced8ab6d27
Pallandor <roger.sejas@gmail.com>
2016-06-29 04:29:18 +0930

    Modify Questions database Schema and add Update Trigger

--- server/db/sql/questions/create.sql
+++ server/db/sql/questions/create.sql
@@ -1,25 +1,23 @@
 /*
     Creates Questions table
+    Includes Trigger which updates updated_at column with current date 
+    whenever a Questions row is updated. 
 */
-
 CREATE EXTENSION IF NOT EXISTS citext;
-
 CREATE TABLE Questions (
 question_id serial PRIMARY KEY,
 category citext NOT NULL,
-content citext NOT NULL,
+question_text citext NOT NULL,
 created_at timestamp NOT NULL DEFAULT now(),
 updated_at timestamp NOT NULL DEFAULT now()
 );
-
--- CREATE FUNCTION update_updated_at_column() RETURNS trigger AS $$
--- BEGIN
--- NEW.updated_at = NOW();
--- RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
--- CREATE TRIGGER questions_updated_at_trigger
--- BEFORE UPDATE ON Questions
--- FOR EACH ROW
--- EXECUTE PROCEDURE update_updated_at_column();
+CREATE OR REPLACE FUNCTION update_updated_at_column() RETURNS trigger AS
+$$
+BEGIN
+NEW.updated_at = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER uuac_questions
+BEFORE UPDATE ON Questions
+FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column(); 

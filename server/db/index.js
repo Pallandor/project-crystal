a18f9e4f8f7af11735b943b212aa258b3e1e4fd7
Pallandor <roger.sejas@gmail.com>
2016-06-29 03:56:19 +0930

    Modify database config to use localhost connection for development

--- server/db/index.js
+++ server/db/index.js
@@ -31,15 +31,15 @@ const options = {
   // database: 'blossomly',
   // user: 'postgres',
 const config = {
-  host: 'ec2-50-112-35-113.us-west-2.compute.amazonaws.com',
-  port: 5432,
-  database: 'blossomly',
-  user: 'other_user',
-  password: 'hottub',
-    // host: 'localhost',
-    // port: 5432,
-    // database: 'blossomly',                       
-    // user: 'postgres',
+//   host: 'ec2-50-112-35-113.us-west-2.compute.amazonaws.com',
+//   port: 5432,
+//   database: 'blossomly',
+//   user: 'other_user',
+//   password: 'hottub',
+    host: 'localhost',
+    port: 5432,
+    database: 'blossomly',                       
+    user: 'postgres',
 };
 
 // Load and initialize pg-promise:
